2d3bd48d872d12616f11f81e14bd984420e23239
Manoj Rayi <manoj.rayi@pratishthanventures.com>
2025-08-06 12:40:11 +0530

    datasource added

---
+++ server/boot/init-db.js
@@ -0,0 +1,13 @@
+
+// eslint-disable-next-line strict
+module.exports = function(app) {
+  const ds = app.dataSources.db; // must match name in datasources.json
+
+  ds.automigrate(['ACL', 'AccessToken', 'Role', 'RoleMapping', 'User'], function(err) {
+    if (err) throw err;
+    console.log('✅ Built-in LoopBack tables created in PostgreSQL.');
+  });
+  if (!ds) {
+    throw new Error('Data source "postgres" not found.');
+  }
+};
