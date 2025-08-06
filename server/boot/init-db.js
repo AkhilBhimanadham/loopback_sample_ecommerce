48871e93294a3ed6916d9abb5764a8d0b37fb60b
Manoj Rayi <manoj.rayi@pratishthanventures.com>
2025-08-06 14:22:28 +0530

    autoupate migrations

--- server/boot/init-db.js
+++ server/boot/init-db.js
@@ -3,7 +3,8 @@
 module.exports = function(app) {
   const ds = app.dataSources.db; // must match name in datasources.json
 
-  ds.automigrate(['ACL', 'AccessToken', 'Role', 'RoleMapping', 'User', 'Product', 'orderItem', 'order'], function(err) {
+  // eslint-disable-next-line max-len
+  ds.autoupdate(['ACL', 'AccessToken', 'Role', 'RoleMapping', 'User', 'Product', 'orderItem', 'order'], function(err) {
     if (err) throw err;
     console.log('✅ Built-in LoopBack tables created in PostgreSQL.');
   });
