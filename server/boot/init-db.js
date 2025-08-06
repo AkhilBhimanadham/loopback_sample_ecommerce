82e1a19aca8e2614f9513e045d3e90c2fa90cbcd
Manoj Rayi <manoj.rayi@pratishthanventures.com>
2025-08-06 14:58:06 +0530

    added users auth

--- server/boot/init-db.js
+++ server/boot/init-db.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   const ds = app.dataSources.db; // must match name in datasources.json
 
   // eslint-disable-next-line max-len
-  ds.autoupdate(['ACL', 'AccessToken', 'Role', 'RoleMapping', 'User', 'Product', 'orderItem', 'order'], function(err) {
+  ds.autoupdate(['ACL', 'AccessToken', 'Role', 'RoleMapping', 'AppUser', 'Product', 'orderItem', 'order'], function(err) {
     if (err) throw err;
     console.log('✅ Built-in LoopBack tables created in PostgreSQL.');
   });
