db13e24933886ed7801fa4754528c5020305fbfb
Bhimanadham Soma Akhilendra Kumar <akhilbhimanadham@Bhimanadhams-MacBook-Pro.local>
2025-08-10 00:44:51 +0530

    final version

--- server/boot/access-token.js
+++ server/boot/access-token.js
@@ -1,21 +1,21 @@
-module.exports = function(app) {
-  const User = app.models.User;
-  const AccessToken = app.models.AccessToken;
+// module.exports = function(app) {
+//   const User = app.models.User;
+//   const AccessToken = app.models.AccessToken;
 
-  AccessToken.observe('access', function(ctx, next) {
-    if (!ctx.accessToken) {
-        console.log("no ctx.accessToken");
-        return next();
+//   AccessToken.observe('access', function(ctx, next) {
+//     if (!ctx.accessToken) {
+//         console.log("no ctx.accessToken");
+//         return next();
 
-    }
+//     }
 
-    User.findById(ctx.accessToken.userId, function(err, user) {
-      if (err) return next(err);
-      if (!user) return next(new Error('No user found for access token'));
+//     User.findById(ctx.accessToken.userId, function(err, user) {
+//       if (err) return next(err);
+//       if (!user) return next(new Error('No user found for access token'));
 
-      ctx.accessToken.user = user; // Attach the full user including role
-      console.log("user role",user.role)
-      next();
-    });
-  });
-};
+//       ctx.accessToken.user = user; // Attach the full user including role
+//       console.log("user role",user.role)
+//       next();
+//     });
+//   });
+// };
